af0a71c0d21e92de765430af43601c9497b61df1
Akhilkumar1f4 <95129443+Akhilkumar1f4@users.noreply.github.com>
2023-06-11 15:48:11 +0530

    Update UC9-EmailSampleValidator.cs

--- UserRegistration/UC9-EmailSampleValidator.cs
+++ UserRegistration/UC9-EmailSampleValidator.cs
@@ -1,12 +1,9 @@
-﻿using System;
-
 public class EmailSampleValidator
 {
     public bool ValidateEmailSample(string emailSample)
     {
-        // Perform email validation logic for the given email sample
+        // Perform email sample validation logic using regular expressions
         // ...
-
         return true; // Return true if the email sample is valid, false otherwise
     }
 }
