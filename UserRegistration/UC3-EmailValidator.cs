ddce5f4997d16d1420db8dcc7511f5af384b9ea5
Akhilkumar1f4 <95129443+Akhilkumar1f4@users.noreply.github.com>
2023-06-11 15:43:39 +0530

    Update UC3-EmailValidator.cs

--- UserRegistration/UC3-EmailValidator.cs
+++ UserRegistration/UC3-EmailValidator.cs
@@ -1,50 +1,9 @@
-﻿using System;
 
 public class EmailValidator
 {
-    public  bool ValidateEmail(string email)
+    public bool ValidateEmail(string email)
     {
-        if (string.IsNullOrEmpty(email))
-        {
-            return false;
-        }
-
-        // Split email into parts
-        string[] parts = email.Split('@');
-        if (parts.Length != 2)
-        {
-            return false;
-        }
-
-        string[] domainParts = parts[1].Split('.');
-        if (domainParts.Length < 2 || domainParts.Length > 3)
-        {
-            return false;
-        }
-
-        string username = parts[0];
-        string domain = domainParts[0];
-        string topLevelDomain = domainParts[1];
-
-        // Check username
-        if (string.IsNullOrEmpty(username))
-        {
-            return false;
-        }
-
-        // Check domain
-        if (string.IsNullOrEmpty(domain))
-        {
-            return false;
-        }
-
-        // Check top-level domain
-        if (string.IsNullOrEmpty(topLevelDomain))
-        {
-            return false;
-        }
-
-        return true;
+        string pattern = @"^[A-Za-z0-9]+([._%+-][A-Za-z0-9]+)*@[A-Za-z0-9]+([.-][A-Za-z0-9]+)*\.[A-Za-z]{2,}$";
+        return Regex.IsMatch(email, pattern);
     }
-
 }
