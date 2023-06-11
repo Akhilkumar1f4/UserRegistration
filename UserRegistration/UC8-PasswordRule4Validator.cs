5e9c36f03c6e55feb1975971dcf27e9e878a2b1c
Akhilkumar1f4 <95129443+Akhilkumar1f4@users.noreply.github.com>
2023-06-11 15:47:30 +0530

    Update UC8-PasswordRule4Validator.cs

--- UserRegistration/UC8-PasswordRule4Validator.cs
+++ UserRegistration/UC8-PasswordRule4Validator.cs
@@ -1,28 +1,8 @@
-﻿using System;
-
 public class PasswordRule4Validator
 {
     public bool ValidateRule4(string password)
     {
-        if (string.IsNullOrEmpty(password))
-        {
-            return false;
-        }
-
-        int specialCharCount = 0;
-        foreach (char c in password)
-        {
-            if (!char.IsLetterOrDigit(c))
-            {
-                specialCharCount++;
-            }
-        }
-
-        if (specialCharCount == 1)
-        {
-            return true;
-        }
-
-        return false;
+        string pattern = @"^(?=.*[@#$%^&+=]).+$";
+        return Regex.IsMatch(password, pattern);
     }
 }
