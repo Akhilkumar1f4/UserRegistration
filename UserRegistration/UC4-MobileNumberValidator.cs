2194e2a01746b43e971b44d0ff2a7ed4ef4cd933
Akhilkumar1f4 <95129443+Akhilkumar1f4@users.noreply.github.com>
2023-06-11 15:44:29 +0530

    Update UC4-MobileNumberValidator.cs

--- UserRegistration/UC4-MobileNumberValidator.cs
+++ UserRegistration/UC4-MobileNumberValidator.cs
@@ -1,41 +1,9 @@
-﻿using System;
 
 public class MobileNumberValidator
 {
-    public  bool ValidateMobileNumber(string mobileNumber)
+    public bool ValidateMobileNumber(string mobileNumber)
     {
-        if (string.IsNullOrEmpty(mobileNumber) || mobileNumber.Length != 12)
-        {
-            return false;
-        }
-
-        // Check country code
-        if (mobileNumber.Substring(0, 2) != "91")
-        {
-            return false;
-        }
-
-        // Check if the remaining part is numeric
-        string remainingDigits = mobileNumber.Substring(3);
-        if (!IsNumeric(remainingDigits))
-        {
-            return false;
-        }
-
-        return true;
+        string pattern = @"^[0-9]{2}\s[0-9]{10}$";
+        return Regex.IsMatch(mobileNumber, pattern);
     }
-
-    private static bool IsNumeric(string input)
-    {
-        foreach (char c in input)
-        {
-            if (!char.IsDigit(c))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-   
-    }
+}
