64f2e7101b58ea228332dd2fbd1df34b87e3172b
Akhilkumar1f4 <95129443+Akhilkumar1f4@users.noreply.github.com>
2023-06-11 15:46:53 +0530

    Update UC7-PasswordRule3Validator.cs

--- UserRegistration/UC7-PasswordRule3Validator.cs
+++ UserRegistration/UC7-PasswordRule3Validator.cs
@@ -1,13 +1,8 @@
-﻿using System;
-
 public class PasswordRule3Validator
 {
     public bool ValidateRule3(string password)
     {
-        if (string.IsNullOrEmpty(password) || !password.Any(char.IsDigit))
-        {
-            return false;
-        }
-        return true;
+        string pattern = @"^(?=.*\d).+$";
+        return Regex.IsMatch(password, pattern);
     }
 }
